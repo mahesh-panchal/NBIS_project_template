4a5f3d6a508bf84eaea23ec3e757088ee954d70a
Mahesh Binzer-Panchal <mahesh.panchal@nbis.se>
2021-05-18 09:32:57 +0200

    Remove version from run script. Assume using shared conda env.

--- analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
+++ analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 NXF_SCRIPT=/proj/snic20XX-YY-ZZ/NBIS_support_<id>/workflow/main.nf
-NXF_VER=21.04.0 nextflow run -c params.config -profile uppmax "$NXF_SCRIPT"
-# Use `nextflow log` to see the time and state of the last nextflow executions. 
+nextflow run -c params.config -profile uppmax "$NXF_SCRIPT"
+# Use `nextflow log` to see the time and state of the last nextflow executions.
