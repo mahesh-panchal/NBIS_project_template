d9a9f9c983d9c9267d9719a29b466df8111190e7
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2021-10-13 16:22:29 +0200

    Update analyses to run from yml

--- analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
+++ analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
-NXF_SCRIPT=/proj/snic20XX-YY-ZZ/NBIS_support_<id>/workflow/main.nf
-nextflow run -c params.config -profile uppmax "$NXF_SCRIPT"
+NXF_SCRIPT="/proj/snic20XX-YY-ZZ/NBIS_support_<id>/workflow/main.nf"
+nextflow run -params-file params.yml -profile uppmax "$NXF_SCRIPT"
+
 # Use `nextflow log` to see the time and state of the last nextflow executions.
