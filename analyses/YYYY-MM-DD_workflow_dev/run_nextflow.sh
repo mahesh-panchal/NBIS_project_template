588822353fd0b255439f18d86180e775749a0b9f
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-01-12 17:39:54 +0100

    Allow profile setting

--- analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
+++ analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
+PROFILE=${PROFILE:-uppmax}
+
 NXF_SCRIPT="/proj/snic20XX-YY-ZZ/NBIS_support_<id>/workflow/main.nf"
-nextflow run -params-file params.yml -profile uppmax "$NXF_SCRIPT"
+nextflow run -params-file params.yml -profile "$PROFILE" "$NXF_SCRIPT"
 
 # Use `nextflow log` to see the time and state of the last nextflow executions.
