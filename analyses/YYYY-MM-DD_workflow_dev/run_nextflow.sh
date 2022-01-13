4b02fb15900b4533666510e855b212e2b28147dc
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-01-13 10:38:27 +0100

    Update nextflow run script to allow path overrides

--- analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
+++ analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
@@ -1,8 +1,16 @@
 #! /usr/bin/env bash
 
 PROFILE=${PROFILE:-uppmax}
+SNIC_COMPUTE=${SNIC_COMPUTE:-/proj/snic20XX-YY-ZZ/}
+SNIC_STORAGE=${SNIC_STORAGE:-/proj/snic20xx-yy-zz/}
+REDMINE_ID=${REDMINE_ID:-XXXX}
+NXF_SCRIPT=${NXF_SCRIPT:-${SNIC_COMPUTE}/NBIS_support_${REDMINE_ID}/workflow/main.nf}
+WORKDIR=${WORKDIR:-$SNIC_STORAGE/nobackup/nxf-work}
 
-NXF_SCRIPT="/proj/snic20XX-YY-ZZ/NBIS_support_<id>/workflow/main.nf"
-nextflow run -params-file params.yml -profile "$PROFILE" "$NXF_SCRIPT"
+nextflow run -resume \
+    -params-file params.yml \
+    -profile "$PROFILE" \
+    -work-dir "$WORKDIR" \
+    "$NXF_SCRIPT"
 
 # Use `nextflow log` to see the time and state of the last nextflow executions.
