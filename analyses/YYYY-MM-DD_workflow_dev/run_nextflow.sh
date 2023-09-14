4e165217fccc0caab7184f0f20816ee53fdf8551
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2023-09-14 14:13:24 +0000

    Update run script changing SNIC to NAISS

--- analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
+++ analyses/YYYY-MM-DD_workflow_dev/run_nextflow.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 PROFILE=${PROFILE:-uppmax}
-SNIC_COMPUTE=${SNIC_COMPUTE:-/proj/snic20XX-YY-ZZ/}
-SNIC_STORAGE=${SNIC_STORAGE:-/proj/snic20xx-yy-zz/}
+NAISS_COMPUTE=${NAISS_COMPUTE:-/proj/naiss20XX-YY-ZZ/}
+NAISS_STORAGE=${NAISS_STORAGE:-/proj/naiss20xx-yy-zz/}
 REDMINE_ID=${REDMINE_ID:-XXXX}
 NXF_SCRIPT=${NXF_SCRIPT:-${SNIC_COMPUTE}/NBIS_support_${REDMINE_ID}/workflow/main.nf}
-WORKDIR=${WORKDIR:-$SNIC_STORAGE/nobackup/nxf-work}
+WORKDIR=${WORKDIR:-$NAISS_STORAGE/nobackup/nxf-work}
 
 nextflow run -resume \
     -params-file params.yml \
